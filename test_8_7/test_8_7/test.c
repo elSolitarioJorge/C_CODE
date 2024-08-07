b11f9ccbe049a829c0e4d59e155c60cfe4c303e4
elSolitarioJorge <Liuhou@outlook.com>
2024-08-07 21:50:07 +0800

    暑假2024.8.7

---
+++ test_8_7/test_8_7/test.c
@@ -0,0 +1,100 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//在屏幕上输入9*9乘法口诀表
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 9; i++)
+//	{
+//		int j = 0;
+//		for(j=1;j<=i;j++)
+//       	printf("%d*%d=%-2d ", i, j, i * j);
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+//实现一个函数，打印乘法口诀表，口诀表的行列数自己指定
+//void print_table(int n)
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 1; i <= n; i++)
+//	{
+//		for (j = 1; j <= i; j++)
+//			printf("%d*%d=%-2d ", i ,j , i * j);
+//		printf("\n");
+//	}
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	print_table(n);
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
