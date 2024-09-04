09a3ac9502e8b1fbdc0d1ddde81c5618b08da3fe
elSolitarioJorge <Liuhou77@outlook.com>
2024-09-04 23:03:26 +0800

    2024.9.4

---
+++ test_9_4/test_9_4/test.c
@@ -0,0 +1,129 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//#include <stdio.h>
+//int main(void)
+//{
+//	int m, t, s;
+//	scanf("%d%d%d", &m, &t, &s);
+//	if (t != 0)
+//	{
+//		if (m > s / t)
+//		{
+//			if (s % t == 0)
+//			{
+//				printf("%d", m - s / t);
+//			}
+//			else
+//			{
+//				printf("%d", m - s / t - 1);
+//			}
+//		}
+//		else
+//		{
+//			printf("0");
+//		}
+//	}
+//	else
+//	{
+//		printf("%d", 0);
+//	}
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//int main(void)
+//{
+//	int num;
+//	scanf("%d", &num);
+//	if (num % 2 == 0 && num > 4 && num <= 12)
+//	{
+//		printf("1 ");
+//	}
+//	else
+//		printf("0 ");
+//	if (num % 2 == 0 || (num > 4 && num <= 12))
+//	{
+//		printf("1 ");
+//	}
+//	else
+//		printf("0 ");
+//	if ((num % 2 == 0 && num <= 4 || num > 12) || (num % 2 != 0 && num > 4 && num <= 12))
+//	{
+//		printf("1 ");
+//	}
+//	else
+//		printf("0 ");
+//	if (num % 2 != 0 && (num <= 4 || num > 12))
+//	{
+//		printf("1 ");
+//	}
+//	else
+//		printf("0 ");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main(void)
+//{
+//	int year;
+//	scanf("%d", &year);
+//	if (year % 4 == 0 && year % 100 != 0)
+//	{
+//		printf("1 ");
+//	}
+//	else if (year % 400 == 0)
+//	{
+//		printf("1 ");
+//	}
+//	else
+//		printf("0 ");
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//int main(void)
+//{
+//	int x;
+//	scanf("%d", &x);
+//	if (x == 0)
+//	{
+//		printf("Today, I ate 0 apple.");
+//	}
+//	else if (x == 1)
+//	{
+//		printf("Today, I ate 1 apple.");
+//	}
+//	else
+//	{
+//		printf("Today, I ate %d apples.", x);
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main(void)
+//{
+//	int n;
+//	scanf("%d", &n);
+//	if (5 * n < 3 * n + 11)
+//	{
+//		printf("Local");
+//	}
+//	else
+//	{
+//		printf("Luogu");
+//	}
+//	return 0;
+//}
+
+ 
+
+
+
+
+
+
+
+
+
+
