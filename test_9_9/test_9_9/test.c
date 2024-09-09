056f4484a9f178e73d4db912bb5bec57ed1cf49a
elSolitarioJorge <Liuhou77@outlook.com>
2024-09-09 23:04:26 +0800

    2024.9.9

---
+++ test_9_9/test_9_9/test.c
@@ -0,0 +1,65 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main(void)
+//{
+//	int n;
+//	int arr[100] = { 0 };
+//	scanf("%d", &n);
+//	for (int i = 0; i < n; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//	}
+//	int min = arr[0];
+//	for (int j = 1; j < n; j++)
+//	{
+//		if (min > arr[j])
+//		{
+//			min = arr[j];
+//		}
+//	}
+//	printf("%d", min);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main(void)
+//{
+//	int n, k, num1=0, num2=0, sum1=0, sum2=0;
+//	scanf("%d%d", &n, &k);
+//	for (int i = 1; i <= n; i++)
+//	{
+//		if (i % k == 0)
+//		{
+//			sum1 += i;
+//			num1++;
+//		}
+//		else
+//		{
+//			sum2 += i;
+//			num2++;
+//		}
+//	}
+//	printf("%.1f %.1f", sum1 / (float)num1, sum2 / (float)num2);
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
