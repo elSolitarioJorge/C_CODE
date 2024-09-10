95c698efe2ca77bd47e34273f5907359b6e823ec
elSolitarioJorge <Liuhou77@outlook.com>
2024-09-10 23:05:05 +0800

    2024.9.10

---
+++ test_9_10/test_9_10/test.c
@@ -0,0 +1,137 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//#include <stdio.h>
+//int main(void)
+//{
+//	int a;
+//	int day = 1;
+//	scanf("%d", &a);
+//	while(a!=1)
+//	{
+//		a /= 2;
+//		day++;
+//	}
+//	printf("%d", day);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main(void)
+//{
+//	int n;
+//	int s = 1;
+//	scanf("%d", &n);
+//	for (int i = n; i > 0; i--)
+//	{
+//		for (int j = i; j > 0; j--)
+//		{
+//			printf("%02d", s++);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main(void)
+//{
+//	double m, h;
+//	scanf("%lf%lf", &m, &h);
+//	double BMI = m / (h * h);
+//	if (BMI < 18.5)
+//	{
+//		printf("Underweight");
+//	}
+//	else if (BMI >= 18.5 && BMI < 24)
+//	{
+//		printf("Normal");
+//	}
+//	else
+//	{
+//		printf("%6g\n", BMI);
+//		printf("Overweight");
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main(void)
+//{
+//	int a, b, c, t;
+//	scanf("%d%d%d", &a, &b, &c);
+//	if (a > b)
+//	{
+//		t = a;
+//		a = b;
+//		b = t;
+//	}
+//	if (a > c)
+//	{
+//		t = a;
+//		a = c;
+//		c = t;
+//	}
+//	if (b > c)
+//	{
+//		t = b;
+//		b = c;
+//		c = t;
+//	}
+//	printf("%d %d %d", a, b, c);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main(void)
+//{
+//	int y, m;
+//	scanf("%d%d", &y, &m);
+//	if ((y % 4 == 0 && y % 100 != 0) || y % 400 == 0)
+//	{
+//		if (m == 2)
+//		{
+//			printf("29");
+//		}
+//		else if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12)
+//		{
+//			printf("31");
+//		}
+//		else
+//		{
+//			printf("30");
+//		}
+//	}
+//	else
+//	{
+//		if (m == 2)
+//		{
+//			printf("28");
+//		}
+//		else if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12)
+//		{
+//			printf("31");
+//		}
+//		else
+//		{
+//			printf("30");
+//		}
+//	}
+//	return 0;
+//}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
