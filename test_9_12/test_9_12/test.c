58932c35c0c01c6aea4a3267765f3ac962c96a6a
elSolitarioJorge <Liuhou77@outlook.com>
2024-09-12 23:07:49 +0800

    2024.9.12

---
+++ test_9_12/test_9_12/test.c
@@ -0,0 +1,80 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//#include <stdio.h>
+//int main(void)
+//{
+//	int a, b, c, t;
+//	char ch[4];
+//	scanf("%d%d%d", &a, &b, &c);
+//	scanf("%s", ch);
+//	if (a > b)
+//	{
+//		t = a;
+//		a = b;
+//		b = t;
+//	}
+//	if (a > c)
+//	{
+//		t = a;
+//		a = c;
+//		c = t;
+//	}
+//	if (b > c)
+//	{
+//		t = b;
+//		b = c;
+//		c = t;
+//	}
+//	for (int i = 0; i < 3; i++)
+//	{
+//		switch(ch[i])
+//		{
+//		case 'A':
+//			printf("%d ", a);
+//			break;
+//		case 'B':
+//			printf("%d ", b);
+//			break;
+//		case 'C':
+//			printf("%d ", c);
+//		}
+//	}
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
