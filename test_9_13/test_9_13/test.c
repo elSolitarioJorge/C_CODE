4dade6aff398eb9835b7612aa28006be77934aa9
elSolitarioJorge <Liuhou77@outlook.com>
2024-09-13 23:08:13 +0800

    2024.9.13

---
+++ test_9_13/test_9_13/test.c
@@ -0,0 +1,78 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//#include <stdio.h>
+//int main(void)
+//{
+//	int n;
+//	scanf("%d", &n);
+//	unsigned long long s = 0;
+//	for (int i = 1; i <= n; i++)
+//	{
+//		long long m = 1;
+//		for (int j = 1; j <= i; j++)
+//		{
+//			m *= j;
+//		}
+//		s += m;
+//	}
+//	printf("%u", s);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main(void)
+//{
+//	int n;
+//	scanf("%u", &n);
+//	unsigned long long s = 0;
+//	for (int i = 1; i <= n; i++)
+//	{
+//		int m = 1;
+//		for (int j = 1; j <= i; j++)
+//		{
+//			m *= j;
+//		}
+//		s += m;
+//	}
+//	printf("%u", s);
+//	return 0;
+//}
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
