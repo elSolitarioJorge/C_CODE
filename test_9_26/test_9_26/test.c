701131a4147eee3e009b991bb609a44e23b19dc4
elSolitarioJorge <Liuhou77@outlook.com>
2024-09-26 22:18:32 +0800

    2024.9.26

---
+++ test_9_26/test_9_26/test.c
@@ -0,0 +1,107 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//#include <stdio.h>
+//void bubble_sort(int arr[], int sz)
+//{
+//	for (int i = 0; i < sz - 1; i++)
+//	{
+//		for (int j = 0; j < sz - 1 - i; j++)
+//		{
+//			if (arr[j] > arr[j + 1])
+//			{
+//				int tem = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = tem;
+//			}
+//		}
+//	}
+//}
+//int main(void)
+//{
+//	int arr[] = { 1,4,5,2,3,7,9,8,10,6 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	bubble_sort(arr, sz);
+//	for (int i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int i;
+//	int a[5] = { 1,2,3,4,5 };
+//	for (int i = 0; i <= 15; ++i) {
+//		a[i] = 0;
+//		printf("6");
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main(void)
+//{
+//	int a[4];
+//	int x = 3;
+//	int* p = &x;
+//	*(&x) = 2;
+//	printf("%d\n", x);
+//	size_t b = sizeof(a+1-1);
+//	printf("%d", b);
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
