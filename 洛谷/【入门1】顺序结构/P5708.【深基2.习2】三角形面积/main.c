d628b3c8ca9642130a48dc937bcee4710de9c646
nihao <2869828375@qq.com>
2025-03-24 21:54:09 +0800

    没时间了，认知实习报告还没写

---
+++ 洛谷/【入门1】顺序结构/P5708.【深基2.习2】三角形面积/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double a, b, c, p;
+    scanf("%lf%lf%lf", &a, &b, &c);
+    p = (a + b + c) / 2;
+    printf("%.1f", sqrt(p * (p - a) * (p - b) * (p - c)));
+    return 0;
+}
